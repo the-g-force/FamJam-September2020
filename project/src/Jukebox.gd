f15cb8ebc7d9add46497aea305741a6b6b13a0b0
Paul Gestwicki <pvgestwicki@bsu.edu>
2020-09-12 13:42:03 -0400

    Add ability to mute music from main menu
    This also involved fixing the path to Grandstander.tres

--- project/src/Jukebox.gd
+++ project/src/Jukebox.gd
@@ -1,5 +1,16 @@
 extends AudioStreamPlayer
 
+onready var _music_bus_index := AudioServer.get_bus_index("Music")
+
+
 func play_gamplay_song():
 	if not playing:
 		play()
+
+
+func mute():
+	AudioServer.set_bus_mute(_music_bus_index, true)
+	
+
+func unmute():
+	AudioServer.set_bus_mute(_music_bus_index, false)
--- project/src/screens/MenuScreen.gd
+++ project/src/screens/MenuScreen.gd
@@ -8,3 +8,10 @@ func _on_PlayButton_pressed():
 func _on_FullscreenButton_pressed():
 	GameStats.reset()
 	OS.window_fullscreen = not OS.window_fullscreen
+
+
+func _on_MuteMusicButton_toggled(button_pressed):
+	if button_pressed:
+		Jukebox.mute()
+	else:
+		Jukebox.unmute()
