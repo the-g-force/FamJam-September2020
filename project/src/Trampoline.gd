64838c9eba51466cf786c9df88241eb77c7179f1
Paul Gestwicki <pvgestwicki@bsu.edu>
2020-09-12 09:42:08 -0400

    Use perfect touch tracking rather than speed

--- project/src/Trampoline.gd
+++ project/src/Trampoline.gd
@@ -2,30 +2,27 @@ extends KinematicBody2D
 
 # Number of pixels around touch point that count as being there
 export var touch_tolerance := 10.0
+# How fast it goes with keyboard input
+export var speed := 200.0
 
-var _speed := 200
 var _is_touch_down := false
 var _touch_position : Vector2
 
 func _process(delta):
-	var direction := Vector2.ZERO
-	
 	# Handle keyboard input
+	var direction := Vector2.ZERO
 	if Input.is_action_pressed("move_left"):
 		direction.x -= 1
 	if Input.is_action_pressed("move_right"):
 		direction.x += 1
+	direction = direction.normalized()
+	var _ignored_result := move_and_collide(direction * speed * delta)
 	
 	# Handle touch input.
 	if _is_touch_down:
-		var distance_to_touch := _touch_position - get_global_transform_with_canvas().origin 
-		if distance_to_touch.x < -touch_tolerance:
-			direction.x -= 1
-		elif distance_to_touch.x > touch_tolerance:
-			direction.x += 1
+		global_position = Vector2(_touch_position.x, global_position.y)
 		
-	direction = direction.normalized()
-	var _ignored_result := move_and_collide(direction * _speed * delta)
+
 	
 
 
