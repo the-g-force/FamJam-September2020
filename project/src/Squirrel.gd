542323fc4f008ba5abd144e6838b52fe9bd11f63
Paul Gestwicki <pvgestwicki@bsu.edu>
2020-09-12 10:09:05 -0400

    Add a collectable 'nut'

--- project/src/Squirrel.gd
+++ project/src/Squirrel.gd
@@ -6,7 +6,11 @@ func _process(delta):
 	var collision := move_and_collide(_velocity * delta)
 	if collision:
 		_velocity = _velocity.bounce(collision.normal)
+		if collision.collider is Collectable:
+			collision.collider.queue_free()
 	update()
+	
+	
 
 func _draw():
 	draw_circle(Vector2.ZERO, $CollisionShape2D.shape.radius, Color.brown)
