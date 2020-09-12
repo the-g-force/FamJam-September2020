c941255a1b70e356771d8cd3ae8965fa73f564aa
Paul Gestwicki <pvgestwicki@bsu.edu>
2020-09-12 09:06:21 -0400

    Move toward mouse or touch input

--- project/src/Trampoline.gd
+++ project/src/Trampoline.gd
@@ -1,15 +1,44 @@
 extends KinematicBody2D
 
-var _speed := 100
+# Number of pixels around touch point that count as being there
+export var touch_tolerance := 10.0
+
+var _speed := 200
+var _is_touch_down := false
+var _touch_position : Vector2
 
 func _process(delta):
 	var direction := Vector2.ZERO
+	
+	# Handle keyboard input
 	if Input.is_action_pressed("move_left"):
 		direction.x -= 1
-	elif Input.is_action_pressed("move_right"):
+	if Input.is_action_pressed("move_right"):
 		direction.x += 1
+	
+	# Handle touch input.
+	if _is_touch_down:
+		var distance_to_touch := _touch_position - get_global_transform_with_canvas().origin 
+		if distance_to_touch.x < -touch_tolerance:
+			direction.x -= 1
+		elif distance_to_touch.x > touch_tolerance:
+			direction.x += 1
+		
 	direction = direction.normalized()
-	move_and_collide(direction * _speed * delta)
+	var _ignored_result := move_and_collide(direction * _speed * delta)
+	
+
+
+func _unhandled_input(event):
+	if event is InputEventMouseButton:
+		if event.pressed:
+			_is_touch_down = true
+			_touch_position = event.position
+		else:
+			_is_touch_down = false
+	elif _is_touch_down and event is InputEventMouseMotion:
+		_touch_position = event.position
+
 
 func _draw():
 	var extents = $CollisionShape2D.shape.extents
